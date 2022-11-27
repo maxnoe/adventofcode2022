8ef943dd3f744893f78cf14e3992121acbf38f32
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-11-27 19:30:49 +0100

    Add exit code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "os"
     "fmt"
     "github.com/maxnoe/adventofcode2022/aoc22"
 )
@@ -9,7 +10,7 @@ func main() {
     input, err := aoc22.GetInput(2021, 1)
     if (err != nil) {
         fmt.Println("ERROR:", err)
-        return
+        os.Exit(1)
     }
     fmt.Println(input)
 }
